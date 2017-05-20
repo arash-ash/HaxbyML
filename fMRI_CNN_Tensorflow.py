25974ea4b0001907fbc414be9c77294080d262fb
Arash <arash.ashrafnejad@gmail.com>
2017-05-21 01:36:14 +0300

    saver code modifier for better organization

--- fMRI_CNN_Tensorflow.py
+++ fMRI_CNN_Tensorflow.py
@@ -119,16 +119,15 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # load the train data
 x_train = np.loadtxt("/home/arash/Dropbox/2017-Spring/CS464/Project/data/subj1/train.txt")
-y_train = np.loadtxt("/home/arash/Dropbox/2017-Spring/CS464/Project/data/subj1/trainLabels.txt")
-
 print(x_train.shape)
+y_train = np.loadtxt("/home/arash/Dropbox/2017-Spring/CS464/Project/data/subj1/trainLabels.txt")
 print(y_train.shape)
 
 # run the CNN
-batch_size = 20
+batch_size = 1 # change 1 back to 20
 sess.run(tf.global_variables_initializer())
 # Include keep_prob in feed_dict to control dropout rate.
-for i in range(100):
+for i in range(1): # change 1 back to 100
     # Shuffle the data
     perm = np.arange(x_train.shape[0])
     np.random.shuffle(perm)
@@ -145,13 +144,16 @@ for i in range(100):
     train_step.run(feed_dict={x: batch_x, y_: batch_y, keep_prob: 0.5})
 
 # for saving the data to tensorboard.
+summary_writer = tf.summary.FileWriter(os.path.join(dir_path, 'plots'), graph=sess.graph)
 saver = tf.train.Saver()
-summary_writer = tf.summary.FileWriter(dir_path, graph=sess.graph)
-saver.save(sess, 'my_trained_model')
+saver.save(sess, "/TrainedModel/model")
 
 # load the test data
-x_train = np.loadtxt("/home/arash/Dropbox/2017-Spring/CS464/Project/data/subj1/test.txt")
-y_train = np.loadtxt("/home/arash/Dropbox/2017-Spring/CS464/Project/data/subj1/testLabels.txt")
+x_test = np.loadtxt("/home/arash/Dropbox/2017-Spring/CS464/Project/data/subj1/test.txt")
+print(x_test.shape)
+y_test = np.loadtxt("/home/arash/Dropbox/2017-Spring/CS464/Project/data/subj1/testLabels.txt")
+print(y_test.shape)
+
 # Evaulate our accuracy on the test data
 print("test accuracy %g"%accuracy.eval(feed_dict={x: x_test, y_: y_test, keep_prob: 1.0}))
 
